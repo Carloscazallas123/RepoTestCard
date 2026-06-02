701b399288c1631a4ae1eb778e432d242e5bff95
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-06-02 14:06:50 +0200

    Creacion del Entorno Back

---
+++ BackEnd/BackEndCards/src/main/java/com/text/demo/BackEndCardsApplication.java
@@ -0,0 +1,13 @@
+package com.text.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndCardsApplication.class, args);
+	}
+
+}
---
+++ BackEnd/BackEndCards/src/test/java/com/text/demo/BackEndCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.text.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
