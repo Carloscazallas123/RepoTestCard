2f74077c6bfece59c4e53368f3890b27ace08705
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-06-14 18:54:30 +0200

    fix: corregir el nombre de la tabla en MatchEntity a TableMatch

--- BackEnd/BackEndCards/src/main/java/com/packs/cards/entitys/MatchEntity.java
+++ BackEnd/BackEndCards/src/main/java/com/packs/cards/entitys/MatchEntity.java
@@ -12,7 +12,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="TableDesk")
+@Table(name="TableMatch")
 public class MatchEntity implements Serializable{
 
 	private static final long serialVersionUID = 1L;
