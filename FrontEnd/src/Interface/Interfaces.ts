ddb3cf62f766ede7c165c67645498582ac544eda
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-06-17 01:10:44 +0200

    fix: asegurar que Player1 y Player2 tengan nombres de usuario en InicioJuego

--- FrontEnd/src/Interface/Interfaces.ts
+++ FrontEnd/src/Interface/Interfaces.ts
@@ -16,8 +16,8 @@ export interface UserDTO {
 
 export interface MatchDTO {
     idMatch?: number;
-    Player1?: UserDTO;
-    Player2?: UserDTO;
+    Player1: UserDTO;
+    Player2: UserDTO;
     State?: string;
     Points1?: number;
     Points2?: number;
