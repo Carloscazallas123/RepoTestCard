6607b922ff7e7c89291ef69d11784d4e9b295b4b
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-06-23 22:54:20 +0200

    fix: corregir la lógica de puntuación y actualización del mazo en función del jugador

--- FrontEnd/src/components/TableroJuego.tsx
+++ FrontEnd/src/components/TableroJuego.tsx
@@ -57,6 +57,7 @@ export const TableroJuego = ()=> {
       Valour: carta.Valour }
 
     JuegoService.escucharJugada(CartaEnviada);
+
       //Esperamos hasta que tengamos dos cartas
       EnBusqueda.current = setInterval(() => {
       const token=localStorage.getItem('Jugada');
@@ -83,6 +84,8 @@ export const TableroJuego = ()=> {
     //Caso que Gana el Jugador 1
     if (Carta1.Valour > Carta2.Valour) {
       P1 = (Partida?.Points1 ?? 0) + 50; 
+      if (Partida?.Player2.Username === miJugador){
+      P2 = (Partida?.Points1 ?? 0) + 50;  }
       setTimeout(() => { 
       setCartaRivalSeleccionada(null); 
       SetCartaSeleccionada(null); }, 3000);}
@@ -90,6 +93,8 @@ export const TableroJuego = ()=> {
     //Caso que Gana el Jugador 2
     if (Carta1.Valour < Carta2.Valour) {
       P2 = (Partida?.Points2 ?? 0) + 50; 
+      if (Partida?.Player2.Username === miJugador){
+      P1 = (Partida?.Points1 ?? 0) + 50;  }
       setTimeout(() => { 
       setCartaRivalSeleccionada(null); 
       SetCartaSeleccionada(null); }, 3000);}
@@ -116,12 +121,11 @@ export const TableroJuego = ()=> {
 
       if (!desk1 || !desk2) return;
 
-      let mazoActualizadoP1 = desk1.Cards?.filter(carta => carta.idCard !== Carta1.idCard);
-      let mazoActualizadoP2 = desk2.Cards?.filter(carta => carta.idCard !== Carta2.idCard);
+      let mazoActualizadoP1 = desk1.Cards?.filter(carta => carta.idCard !== Carta1.idCard || 
+                                                  carta.idCard !== Carta2.idCard );
 
-      if (Partida?.Player2.Username === miJugador) {
-      mazoActualizadoP1 = desk1.Cards?.filter(carta => carta.idCard !== Carta2.idCard);
-      mazoActualizadoP2 = desk2.Cards?.filter(carta => carta.idCard !== Carta1.idCard); }
+      let mazoActualizadoP2 = desk2.Cards?.filter(carta => carta.idCard !== Carta1.idCard ||
+                                                  carta.idCard !== Carta2.idCard );
 
       //Perspectiva 1
       SetPartida({
