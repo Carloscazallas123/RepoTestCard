1e57a3570965cf35b5fa2fd9371ecfb9f7aa9b1f
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-06-23 21:39:16 +0200

    fix: corregir la comparación del ID de la partida en la lógica de selección de carta rival

--- FrontEnd/src/components/TableroJuego.tsx
+++ FrontEnd/src/components/TableroJuego.tsx
@@ -62,7 +62,8 @@ export const TableroJuego = ()=> {
       const token=localStorage.getItem('Jugada');
       if(token){
         const Jugada: GameDTO = JSON.parse(token);
-        if(Partida?.IdMatch === Jugada.idMatch){
+        if(Partida?.IdMatch == Jugada.idMatch){
+          console.log('33333333');
           setCartaRivalSeleccionada(Jugada.card2);
           if (Partida?.Player2.Username === miJugador){
           setCartaRivalSeleccionada(Jugada.card1); }
