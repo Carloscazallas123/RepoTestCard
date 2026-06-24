6abb56989c7b9f1e8faac44684dfc84c0fe6329e
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-06-24 18:41:38 +0200

    fix: reorganizar lógica de finalización de partida para mejorar la eliminación de cartas y usuarios

--- BackEnd/BackEndCards/src/main/java/com/packs/cards/controllers/PrincipalController.java
+++ BackEnd/BackEndCards/src/main/java/com/packs/cards/controllers/PrincipalController.java
@@ -121,13 +121,25 @@ public class PrincipalController {
 	@SendTo("/topic/Terminar")
 	@Transactional
 	public synchronized MatchDTO FinishMatch(MatchDTO Partida) {
-	//Borrando La Partida
-	MatchEntity EntityMatch = MatchRepo.ObtenerporId(Partida.getIdMatch());
-	EntityMatch.setPlayer1(null); EntityMatch.setPlayer2(null);
-	EntityMatch.setPoints1(null); EntityMatch.setPoints2(null);
-	EntityMatch.setState(null); MatchRepo.save(EntityMatch);
-	MatchRepo.deleteById(Partida.getIdMatch());
-	System.out.println(Partida.getIdMatch() + " Eliminada");
+		
+	//Borrando las Cartas de Ambos Jugadores
+	for(int i=0;i<Partida.getPlayer1().getDeskUser().getCards().size() || i < Partida.getPlayer2().getDeskUser().getCards().size() ; i++) {
+			CardsRepo.deleteById(Partida.getPlayer1().getDeskUser().getCards().get(i).getIdCard());
+			CardsRepo.deleteById(Partida.getPlayer2().getDeskUser().getCards().get(i).getIdCard());
+	} System.out.println("Cartas Eliminadas");
+	
+	//Borrando el Mazo 1
+	DeskEntity D1 = DeskRepo.ObtenerporId(Partida.getPlayer1().getDeskUser().getIdDesk());
+	D1.setNameDesk(null); D1.setDeskCards(null); DeskRepo.save(D1);
+		
+	//Borrando el Mazo 2
+	DeskEntity D2 = DeskRepo.ObtenerporId(Partida.getPlayer2().getDeskUser().getIdDesk());
+	D2.setNameDesk(null); D2.setDeskCards(null); DeskRepo.save(D2);
+		
+	DeskRepo.deleteById(Partida.getPlayer2().getDeskUser().getIdDesk());
+	System.out.println(Partida.getPlayer1().getDeskUser().getNameDesk() + " Eliminado");
+	System.out.println(Partida.getPlayer2().getDeskUser().getNameDesk() + " Eliminado");
+	
 	//Borrando los Usuarios
 	UserEntity P1 = UserRepo.ObtenerporId(Partida.getPlayer1().getIduser());
 	UserEntity P2 = UserRepo.ObtenerporId(Partida.getPlayer1().getIduser());
@@ -139,26 +151,13 @@ public class PrincipalController {
 	System.out.println(Partida.getPlayer1().getUsername() + " Eliminado");
 	System.out.println(Partida.getPlayer2().getUsername() + "Eliminado");
 	
-	
-	//Borrando el Mazo 1
-	DeskEntity D1 = DeskRepo.ObtenerporId(Partida.getPlayer1().getDeskUser().getIdDesk());
-	D1.setNameDesk(null); D1.setDeskCards(null); DeskRepo.save(D1);
-	
-	//Borrando el Mazo 2
-	DeskEntity D2 = DeskRepo.ObtenerporId(Partida.getPlayer2().getDeskUser().getIdDesk());
-	D2.setNameDesk(null); D2.setDeskCards(null); DeskRepo.save(D2);
-	
-	DeskRepo.deleteById(Partida.getPlayer2().getDeskUser().getIdDesk());
-	System.out.println(Partida.getPlayer1().getDeskUser().getNameDesk() + " Eliminado");
-	System.out.println(Partida.getPlayer2().getDeskUser().getNameDesk() + " Eliminado");
-	
-	//Borrando las Cartas de Ambos Jugadores
-	for(int i=0;i<Partida.getPlayer1().getDeskUser().getCards().size()
-			 || i < Partida.getPlayer2().getDeskUser().getCards().size() ; i++) {
-		
-		CardsRepo.deleteById(Partida.getPlayer1().getDeskUser().getCards().get(i).getIdCard());
-		CardsRepo.deleteById(Partida.getPlayer2().getDeskUser().getCards().get(i).getIdCard());
-	} System.out.println("Cartas Eliminadas");
+	//Borrando La Partida
+	MatchEntity EntityMatch = MatchRepo.ObtenerporId(Partida.getIdMatch());
+	EntityMatch.setPlayer1(null); EntityMatch.setPlayer2(null);
+	EntityMatch.setPoints1(null); EntityMatch.setPoints2(null);
+	EntityMatch.setState(null); MatchRepo.save(EntityMatch);
+	MatchRepo.deleteById(Partida.getIdMatch());
+	System.out.println(Partida.getIdMatch() + " Eliminada");
 	
 	//Borrando las Jugadas
 	List<GameEntity>ListaJugadas=RepoGame.Obtenerporpartida(Partida.getIdMatch());
